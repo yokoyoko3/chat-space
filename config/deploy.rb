878aab8e1968627082af363c9ae50f37675842e4
yokoyoko3 <57942559+yokoyoko3@users.noreply.github.com>
2019-12-07 12:58:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,16 +36,16 @@ set :default_env, {
 }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-    desc 'upload secrets.yml'
-    task :upload do
-      on roles(:app) do |host|
-        if test "[ ! -d #{shared_path}/config ]"
-          execute "mkdir -p #{shared_path}/config"
-        end
-        upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
       end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
-    before :starting, 'deploy:upload'
-    after :finishing, 'deploy:cleanup'
   end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+#end
